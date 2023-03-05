b9d0752fe794cf75f445da0b65f47b199d68bfb9
Ernest Cui <ecuiships@126.com>
2023-03-05 16:45:22 +0800

    UI interaction complete

--- Lab1/ConnectRadar/MainWindow.xaml.cs
+++ Lab1/ConnectRadar/MainWindow.xaml.cs
@@ -20,9 +20,114 @@ namespace ConnectRadar
     /// </summary>
     public partial class MainWindow : Window
     {
+        ParseHelper parseHelper;
+        PortScanManager portScanManager;
         public MainWindow()
         {
+            parseHelper = new ParseHelper();
+            portScanManager = new PortScanManager();
+
             InitializeComponent();
+        }
+
+        private void OnTbIpComponentTextChanged(
+            TextBox current, TextBox? next, bool isStart, int currentIndex)
+        {
+            int parsedComponent = parseHelper.ParseIp4AddressComponent(current.Text);
+            current.Text = parsedComponent.ToString();
+            portScanManager.SetIp4AddressComponent(isStart, currentIndex, parsedComponent);
+
+            if (next != null && current.Text.Length >= 3)
+            {
+                next.Focus();
+                next.SelectAll();
+            }
+        }
+
+        private void tbIpStart1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            OnTbIpComponentTextChanged(tbIpStart1, tbIpStart2, true, 3);
+        }
+
+        private void tbIpStart2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            OnTbIpComponentTextChanged(tbIpStart2, tbIpStart3, true, 2);
+        }
+
+        private void tbIpStart3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            OnTbIpComponentTextChanged(tbIpStart3, tbIpStart4, true, 1);
+        }
+
+        private void tbIpStart4_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            OnTbIpComponentTextChanged(tbIpStart4, tbIpEnd1, true, 0);
+        }
+
+        private void tbIpEnd1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            OnTbIpComponentTextChanged(tbIpEnd1, tbIpEnd2, false, 3);
+        }
+
+        private void tbIpEnd2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            OnTbIpComponentTextChanged(tbIpEnd2, tbIpEnd3, false, 2);
+        }
+
+        private void tbIpEnd3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            OnTbIpComponentTextChanged(tbIpEnd3, tbIpEnd4, false, 1);
+        }
+
+        private void tbIpEnd4_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            OnTbIpComponentTextChanged(tbIpEnd4, null, false, 0);
+        }
+
+        private void tbPortStart_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int parsedPort = parseHelper.ParsePort(tbPortStart.Text);
+            tbPortStart.Text = parsedPort.ToString();
+            portScanManager.SetPortStart(parsedPort);
+        }
+
+        private void tbPortEnd_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int parsedPort = parseHelper.ParsePort(tbPortEnd.Text);
+            tbPortEnd.Text = parsedPort.ToString();
+            portScanManager.SetPortEnd(parsedPort);
+        }
+
+        private void tbThreadCount_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            int parsedThreadCount = parseHelper.ParseThreadCount(tbThreadCount.Text);
+            tbThreadCount.Text = parsedThreadCount.ToString();
+            portScanManager.SetThreadCount(parsedThreadCount);
+        }
+
+        private void btnThreadDec_Click(object sender, RoutedEventArgs e)
+        {
+            tbThreadCount.Text = (int.Parse(tbThreadCount.Text) - 1).ToString();
+        }
+
+        private void btnThreadInc_Click(object sender, RoutedEventArgs e)
+        {
+            tbThreadCount.Text = (int.Parse(tbThreadCount.Text) + 1).ToString();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            string message;
+            if(!portScanManager.CheckScanParameters(out message))
+            {
+                MessageBox.Show($"【错误】{message}", "错误");
+                return;
+            }
+        }
+
+        private void btnEnd_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Lab1/ConnectRadar/PortScanManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectRadar
+{
+    internal class PortScanManager
+    {
+        private uint ip4AddressStart;
+        private uint ip4AddressEnd;
+
+        private int portStart;
+        private int portEnd;
+
+        private int threadCount;
+
+        public void SetIp4AddressComponent(bool isStart, int index, int component)
+        {
+            unsafe
+            {
+                fixed (uint* addressStartPtr = &ip4AddressStart,
+                    addressEndPtr = &ip4AddressEnd)
+                {
+                    uint* addressPtr = isStart ? addressStartPtr : addressEndPtr;
+                    *((byte*)addressPtr + index) = (byte)component;
+                }
+            }
+        }
+
+        public void SetPortStart(int portStart)
+        {
+            this.portStart = portStart;
+        }
+
+        public void SetPortEnd(int portEnd)
+        {
+            this.portEnd = portEnd;
+        }
+
+        public void SetThreadCount(int threadCount)
+        {
+            this.threadCount = threadCount;
+        }
+
+        public bool CheckScanParameters(out string message)
+        {
+            if (ip4AddressStart > ip4AddressEnd)
+            {
+                message = "起始IPv4地址大于结束IPv4地址";
+                return false;
+            }
+
+            if (portStart > portEnd)
+            {
+                message = "起始端口号大于结束端口号";
+                return false;
+            }
+
+            message = "";
+            return true;
+        }
+    }
+}
