dc815bb43d452b7ade015e2f8134f027c7673482
Ernest Cui <ecuiships@126.com>
2023-03-05 10:20:15 +0800

    paeseHelper complete

---
+++ Lab1/ConnectRadar/ParseHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectRadar
+{
+    internal class ParseHelper
+    {
+        private const int IP4_ADDRESS_COMPONENT_MIN = 0;
+        private const int IP4_ADDRESS_COMPONENT_MAX = 255;
+
+        private const int PORT_MIN = 0;
+        private const int PORT_MAX = 65535;
+
+        private const int THREAD_COUNT_MIN = 1;
+        private const int THREAD_COUNT_MAX = 9999;
+
+        private int ParseRange(string value,int min,int max)
+        {
+            int parsedValue;
+
+            if (!int.TryParse(value, out parsedValue))
+            {
+                return 0;
+            }
+
+            if (parsedValue < min)
+            {
+                return min;
+            }
+            else if (parsedValue > max)
+            {
+                return max;
+            }
+            else
+            {
+                return parsedValue;
+            }
+        }
+
+        public int ParseIp4AddressComponent(string component)
+        {
+            return ParseRange(
+                component, IP4_ADDRESS_COMPONENT_MIN, IP4_ADDRESS_COMPONENT_MAX);
+        }
+
+        public int ParsePort(string port)
+        {
+            return ParseRange(port, PORT_MIN, PORT_MAX);
+        }
+
+        public int ParseThreadCount(string threadCount)
+        {
+            return ParseRange(threadCount, THREAD_COUNT_MIN, THREAD_COUNT_MAX);
+        }
+    }
+}
